48ec06caf1fb122f27f41d31022bc6c770d8386e
Matheus <Binotto>
2019-06-03 20:10:28 -0300

    Creating the check functions - houses equal / end of game

--- js/app.js
+++ js/app.js
@@ -7,7 +7,34 @@ let playerX = 0;
 let playerO = 0;
 
 
-
+const housesAlike = (a, b, c) => {
+    const backgroundA = $("#house"+a).css("background-image");
+    const backgroundB = $("#house"+b).css("background-image");
+    const backgroundC = $("#house"+c).css("background-image");
+    if( (backgroundA == backgroundB) && (backgroundB == backgroundC) && (backgroundA != "none" && backgroundA != "")){
+        if($('#house'+a).hasClass('playerX')){
+            winner = 'X';
+            playerX++;
+            $("#X").html(`Player ${winner} : ${playerX}`);
+        }else if($('#house'+a).hasClass('playerO')){           
+            winner = 'O';
+            playerO++;
+            $("#O").html(`Player ${winner} : ${playerO}`);
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+const endOfTheGame = () => {
+    if( housesAlike('One', 'Two', 'Three') || housesAlike('Four', 'Five', 'Six') || housesAlike('Seven', 'Eight', 'Nine') ||
+        housesAlike('One', 'Four', 'Seven') || housesAlike('Two', 'Five', 'Eight') || housesAlike('Three', 'Six', 'Nine') ||
+        housesAlike('One', 'Five', 'Nine') || housesAlike('Three', 'Five', 'Seven')
+    ){
+        initiated = 1;
+    }
+}
 
 
 
